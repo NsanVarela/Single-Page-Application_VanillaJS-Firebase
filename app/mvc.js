9a4275d0ff1118bd7e8e783b0e70e70c5af2ebef
NSanVarela <nsanvarela@gmail.com>
2018-10-31 14:08:21 +0100

    form

--- app/mvc.js
+++ app/mvc.js
@@ -1,14 +1,48 @@
-/* Fonctions exportables pour alimenter la librairie MVC */
+let router = null;
 
-export function renderView(htmlFilename)
+export function addRoute (url, controller, template, view)
 {
 
-    fetch('./views/hello.html')
+    router.add(url, function () 
+    {
+        if (controller === Object(controller) && controller.run)
+        // on vérifie que la variable est un objet & qu'une propriété existe
+        {
+            controller.run();
+        }
+        
+        if (template !== undefined)
+        {
+            renderTemplate(template);
+        }
+    });
+};
+
+
+export function initialize()
+{
+    router = new Router({
+        mode: 'hash'
+    });
+};
+
+function renderTemplate(htmlFileName)
+{
+    
+    window.fetch(`./views/${htmlFileName}`)
     .then(function(response) {
       return response.text(); // retourne la réponse http brut sous forme d'objet
     })
-    .then(function(template) {
-        document.querySelector('main').innerHTML=template;
+    .then(function(htmlTemplate) {
+        document.querySelector('main').innerHTML=htmlTemplate;
     });
+};
 
+export function run ()
+{
+    if(router === null)
+    {
+        throw new Error("Cette route n'existe pas");
+    }
+    router.addUriListener().check();
 }
---
+++ controllers/SignupController.js
@@ -0,0 +1,7 @@
+export default class signupController
+{
+    run()
+    {
+        console.log('un controller');
+    }
+}
